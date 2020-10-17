e8443316e91037f63da3d40d7893e53be12c14e7
jpacerqueira <jpacerqueira@gmail.com>
2020-10-17 13:36:39 +0100

    initial commit

---
+++ start-nordVPN-instance-docker.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+#
+NORD_EMAIL=$1
+NORD_PASSWD=$2 ## 'PASSWORD'
+NORD_COUNTRY=country ## 'France'
+#
+docker run -ti --cap-add=NET_ADMIN --cap-add=SYS_MODULE --device /dev/net/tun --name vpn \
+            --sysctl net.ipv4.conf.all.rp_filter=2 \
+            -e USER=${NORD_EMAIL} -e PASS=${NORD_PASSWD} \
+            -e CONNECT=${NORD_COUNTRY} -e TECHNOLOGY=NordLynx -d jpacerqueira83/nordvpn
+#
+ECHO ">> include VPN in all docker container runs"
+ECHO "$$ docker run -it --net=container:vpn -d some/docker-container "
+#
